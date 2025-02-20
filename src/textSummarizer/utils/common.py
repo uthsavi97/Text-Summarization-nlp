d773ba6bdedf534b625743f3a220dd1c9afb0385
Uthsavi YP <uthsaviyp@gmail.com>
2025-02-20 23:01:23 +0530

    init

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -5,20 +5,21 @@ from textSummarizer.logging import logger
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
-from typing import Any 
+from typing import Any
+
 
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
-    
+
     Args:
-        path_to_yaml (str); path like input
-        
+        path_to_yaml (str): path like input
+
     Raises:
         ValueError: if yaml file is empty
         e: empty file
-        
+
     Returns:
         ConfigBox: ConfigBox type
     """
@@ -61,3 +62,5 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
+
+    

5b7d81cad035a6474ed03e53220fce8fd77012ae
Uthsavi YP <uthsaviyp@gmail.com>
2025-03-02 08:47:13 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from textSummarizer.logging import logger
+from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
